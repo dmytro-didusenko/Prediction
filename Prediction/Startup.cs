0731259465d5e179962ed0af5a3a62046b01ef7a
didusenko <didusenko.dmytro@gmail.com>
2020-11-22 02:55:47 +0200

    Added Swagger to API project

--- Prediction/Startup.cs
+++ Prediction/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Prediction
 {
@@ -31,6 +32,11 @@ namespace Prediction
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Prediction", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +52,13 @@ namespace Prediction
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Prediction API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
